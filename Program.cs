8d0cc8c93670ec52c78020dffaa0baec2c2d8bdd
Deilons <apolo47478@gmail.com>
2024-07-25 08:33:59 -0500

    feat: prueba de los metodos en las clases

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using taller_Escuela.Models;
+
+var administradorApp = new AdministradorApp();
+
+administradorApp.AgregarProfesor(new Profesor()
+{   
+    Id = 1,
+    Nombre = "Juan",
+    Apellidos = "Perez",
+    TipoDocumento = "DNI",
+    NroDocumento = "12345678",
+    Email = "j@j.com",
+    Telefono = "123456789",
+    Asignatura = "Programacion",
+    Sueldo = 5000,
+    FechaContrato = new DateTime(2021, 11, 11),
+    Cursos = new List<string> { "Programacion", "Ingles" }
+});
+
+administradorApp.MostrarProfesores();
+
+
+administradorApp.Profesores[0].ObtenerSueldo();
+
+Console.WriteLine("-----------------------");
+
+administradorApp.AgregarEstudiante(new Estudiante()
+{
+    Id = 1,
+    Nombre = "Maria",
+    Apellidos = "Gonzalez",
+    TipoDocumento = "DNI",
+    NroDocumento = "12345678",
+    Email = "m@m.com",
+    Telefono = "123456789",
+    NombreAcudiente = "Pedro",
+    CursoActual = "Programacion",
+    Calificaciones = new List<double> { 8.5, 9.0, 7.5 }
+});
+
+administradorApp.MostrarEstudiantes();
+
+Console.WriteLine("-----------------------");
+
+
+administradorApp.Estudiantes[0].CalcularPromedio();
